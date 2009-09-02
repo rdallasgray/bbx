f4af7572919db7defbb7ded30e34bbb383b3b63e
robertjohnston <robertjohnston@8c6fc3d7-bd60-4b29-84f5-874647381e5f>
2009-09-02 17:27:33 +0000

    git-svn-id: file://localhost/Users/robertdallasgray/svn/Backbox/trunk@23 8c6fc3d7-bd60-4b29-84f5-874647381e5f

---
+++ Loader.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+
+Copyright (c) 2009 Robert Johnston
+
+This file is part of Backbox.
+
+Backbox is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software  Foundation, either version 3 of the License, or (at your option) any later version.
+ 
+Boxes is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with Backbox. If not, see http://www.gnu.org/licenses/.
+
+*/
+
+
+
+class Bbx_Loader extends Zend_Loader {
+
+	public static function loadClass($class,$dirs = null) {
+		parent::loadClass($class,$dirs);
+	}
+
+	public static function autoload($class) {
+		try {
+			self::loadClass($class,array(
+				SHARED_LIB.'/Bbx/Vendor',
+				SITE_ROOT.'/library',
+				SHARED_LIB,
+				SITE_ROOT.'/application/modules/'.MODULE_NAME.'/models'
+			));
+			return $class;
+		} 
+		catch (Exception $e) {
+			return false;
+		}
+	}
+
+}
+
+?>
