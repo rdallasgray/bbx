cde3c1ad57fbc8a6b18d249329990d43f42f9ea1
robertjohnston <robertjohnston@8c6fc3d7-bd60-4b29-84f5-874647381e5f>
2009-12-15 12:01:05 +0000

    REmoved most utf8 funcs and replace with mbstring
    git-svn-id: file://localhost/Users/robertdallasgray/svn/Backbox/trunk@133 8c6fc3d7-bd60-4b29-84f5-874647381e5f

--- Common/utf8.php
+++ Common/utf8.php
@@ -15,21 +15,6 @@ You should have received a copy of the GNU General Public License along with Bac
 */
 
 
-
-function mb_exists() {
-	if (Zend_Registry::isRegistered('MB_EXISTS')) {
-		return Zend_Registry::get('MB_EXISTS');
-	}
-	else {
-		if (function_exists('mb_substr')) {
-			Zend_Registry::set('MB_EXISTS',true);
-			return true;
-		}
-		Zend_Registry::set('MB_EXISTS',false);
-		return false;
-	}
-}
-
 function utf8_str_replace($search,$replace,$subject) {
 	$subject = utf8_decode($subject);
 	if (!is_array($search)) {
@@ -44,34 +29,6 @@ function utf8_str_replace($search,$replace,$subject) {
 	
 }
 
-function utf8_substr($str,$start) {
-	if (mb_exists()) {
-		if(func_num_args() >= 3) {
-			$end = func_get_arg(2);
-			return mb_substr($str,$start,$end);
-		}
-		else {
-			return mb_substr($str,$start);
-		}
-	}
-	preg_match_all("/./u",$str,$ar);
-
-	if(func_num_args() >= 3) {
-		$end = func_get_arg(2);
-		return join("",array_slice($ar[0],$start,$end));
-	}
-	else {
-		return join("",array_slice($ar[0],$start));
-	}
-}
-
-function utf8_strlen($str) {
-	if (mb_exists()) {
-		return mb_strlen($str);
-	}
-	return preg_match_all('/[\x00-\x7F\xC0-\xFD]/', $str, $dummy);
-}
-
 function utf8_ucfirst($str){
 	$str=utf8_decode($str);
 	$str=ucfirst($str);
@@ -80,13 +37,7 @@ function utf8_ucfirst($str){
 }
 
 function utf8_ucwords($str){
-	if (mb_exists()) {
-		return mb_convert_case($str,MB_CASE_TITLE);
-	}
-	$str=utf8_decode($str);
-	$str=ucwords($str);
-	$str=utf8_encode($str);
-	return $str;
+	return mb_convert_case($str,MB_CASE_TITLE);
 }
 
 function utf8_titlecase($str){
@@ -107,26 +58,6 @@ function utf8_titlecase($str){
     $str = ucfirst(join(" ", $newwords));
 	$str = utf8_encode($str);
 	return $str;
-}
-
-function utf8_strtoupper($str){
-	if (mb_exists()) {
-		return mb_strtoupper($str);
-	}
-	$string=utf8_decode($str);
-	$string=strtoupper($str);
-	$string=utf8_encode($str);
-	return $string;
-}
-
-function utf8_strtolower($str){
-	if (mb_exists()) {
-		return mb_strtolower($str);
-	}
-	$string=utf8_decode($str);
-	$string=strtolower($str);
-	$string=utf8_encode($str);
-	return $string;
 }
 
 function utf8_str_pad($input,$pad_length,$pad_string = '',$pad_type = 1,$charset = "UTF-8") {
@@ -156,10 +87,10 @@ function utf8_str_pad($input,$pad_length,$pad_string = '',$pad_type = 1,$charset
 }
 
 function utf8_excerpt($text,$chars,$balance_tags = true) {
-	if (utf8_strlen($text) > $chars) {
+	if (mb_strlen($text) > $chars) {
 		$text = $text." ";
-		$text = utf8_substr($text,0,$chars);
-		$text = utf8_substr($text,0,utf8_strrpos($text,' '));
+		$text = mb_substr($text,0,$chars);
+		$text = mb_substr($text,0,mb_strrpos($text,' '));
 		$text = $text." ...";
 	}
 	if ($balance_tags) {
@@ -167,14 +98,5 @@ function utf8_excerpt($text,$chars,$balance_tags = true) {
 	}
 	return $text;
 }
-
-function utf8_strrpos($text,$str) {
-	if (mb_exists()) {
-		return mb_strrpos($text,$str);
-	}
-	$text = utf8_decode($text);
-	return strrpos($text,$str);
-}
-
 
 ?>
