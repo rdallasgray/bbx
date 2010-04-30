faaf7cc508d736fa47b3255b767f773291f3bf07
robertdallasgray <robertdallasgray@8c6fc3d7-bd60-4b29-84f5-874647381e5f>
2010-04-30 07:47:53 +0000

    Added Model view helper
    git-svn-id: file://localhost/Users/robertdallasgray/svn/Backbox/trunk@306 8c6fc3d7-bd60-4b29-84f5-874647381e5f

---
+++ View/Helper/Model.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+
+Copyright (c) 2009 Robert Johnston
+
+This file is part of Backbox.
+
+Backbox is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software  Foundation, either version 3 of the License, or (at your option) any later version.
+ 
+Backbox is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with Backbox. If not, see http://www.gnu.org/licenses/.
+
+*/
+
+class Bbx_View_Helper_Model extends Zend_View_Helper_Abstract {
+	
+	protected $_model;
+	protected $_modelName;
+	
+	public function setView(Zend_View_Interface $view) {
+		$this->view = $view;
+	}
+	
+	public function __call($method, $arguments) {
+		
+		$this->_modelName = $method;
+		
+		$m = (count($arguments) > 0) ? $arguments[0] : null;
+		
+		if ($m === null) {
+			$this->_model = $this->view->{$this->_modelName};
+		}
+		else {
+			$this->_model = $m;
+		}
+		
+		return $this;		
+	}
+	
+}
+
+?>
