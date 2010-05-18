b1343a1ebcd9619f1e0b657a57ea2f21c755e39d
robertdallasgray <robertdallasgray@8c6fc3d7-bd60-4b29-84f5-874647381e5f>
2010-05-18 15:28:54 +0000

    Added Search
    git-svn-id: file://localhost/Users/robertdallasgray/svn/Backbox/trunk@328 8c6fc3d7-bd60-4b29-84f5-874647381e5f

--- Search.php
+++ Search.php
@@ -18,27 +18,29 @@ You should have received a copy of the GNU General Public License along with Bac
 
 class Bbx_Search {
 	
-	protected $_front;
 	protected $_index;
-	protected $_fields = array();
-	protected $_indexInterval = 21600;
-	protected $_docId;
-	protected $_document;
-	protected $_query;
-	protected $_indexLevels;
 	
 	public static function reset() {
-		Zend_Search_Lucene::create(SITE_ROOT.'/search/index');
+		$moduleName = Zend_Controller_Front::getInstance()->getRequest()->getModuleName();
+		Zend_Search_Lucene::create(APPLICATION_PATH . '/modules/' . $moduleName . '/search/index');
+	}
+	
+	public function __construct() {
+		Zend_Search_Lucene_Analysis_Analyzer::setDefault(
+			new Zend_Search_Lucene_Analysis_Analyzer_Common_Utf8Num_CaseInsensitive());
+		$moduleName = Zend_Controller_Front::getInstance()->getRequest()->getModuleName();
+		$indexPath = APPLICATION_PATH . '/modules/' . $moduleName . '/search/index';
+		if (file_exists($indexPath)) {
+			$this->_index = Zend_Search_Lucene::open($indexPath);
+		}
+		else {
+			$this->_index = Zend_Search_Lucene::create($indexPath);
+		}
 	}
 	
 	public static function find($query) {
 		$search = new Bbx_Search;
 		return $search->_doFind($query);
-	}
-	
-	public function __construct() {
-//		Zend_Search_Lucene_Analysis_Analyzer::setDefault(new Zend_Search_Lucene_Analysis_Analyzer_Common_Utf8());
-		$this->_getIndex();
 	}
 		
 	public function optimize() {
@@ -52,74 +54,58 @@ class Bbx_Search {
 	public function count() {
 		return $this->_index->numDocs();
 	}
-	
-	protected function _report($str) {
-		echo $str."\n";
-	}
 
 	protected function _doFind($query) {
 		$this->_query = $query;
 		return $this->_prepareResults();
 	}
 	
-	public function index($newDoc,$domDoc,$url) {
-		$this->_fields['url'] = urlencode($url);
-		$docId = $this->_fields['url'];
-		if (Zend_Registry::isRegistered('locale') && $lang = $domDoc->documentElement->getAttribute('lang')) {
-			$this->_report('Setting language to '.$lang);
-			$this->_fields['lang'] = $lang;
-			$docId .= urlencode('?lang='.$this->_fields['lang']);
-		}
-		$this->_fields['docId'] = md5($docId);
-		if ($oldDoc = $this->_checkDocumentExists($this->_fields['docId'])) {
-			if (md5($newDoc->body) == $oldDoc->checkSum) {
-				$this->_report('No change in document');
+	public function indexDoc($doc, $url) {
+		$fields = array();
+		$fields['url'] = urlencode($url);
+		$fields['docId'] = md5($fields['url']);
+		if ($oldDoc = $this->_documentExists($fields['docId'])) {
+			if (md5($doc->body) == $oldDoc->checkSum) {
+				Bbx_Log::debug('No change in document');
 				return;
 			}
 			else {
 				$this->_deleteDocument($oldDoc->id);
 			}
 		}
-		$this->_indexDocument($newDoc,$domDoc);
+		$html = $doc->getHTML();
+		$contentStart = strpos($html, '<section id="main-content">');
+		$contentEnd = strpos($html, '</section>') - $contentStart;
+		if ($contentStart !== false) {
+			$html = substr($html, $contentStart, $contentEnd);
+		}
+		$html = str_replace(array('</', '<br'), array(' </', ' <br'), $html);
+		$html = strip_tags($html);
+		$contentSample = preg_replace('/[\t\s\n]+/', ' ', $html);
+		$fields['contentSample'] = utf8_excerpt(trim($contentSample), 128, false);
+		$this->_indexDocument($doc, $fields);
 	}
 	
-	protected function _indexDocument($searchDoc,$domDoc) {
-		$contentSample = @$domDoc->getElementById('main')->getElementsByTagName('p')->item(0)->textContent;
-		$contentHead1 = @$domDoc->getElementById('main')->getElementsByTagName('h1')->item(0)->textContent;
-		$contentHead2 = @$domDoc->getElementById('main')->getElementsByTagName('h2')->item(0)->textContent;
-		
-		$searchDoc->addField(Zend_Search_Lucene_Field::UnIndexed('checkSum',md5($searchDoc->body)));
-		$searchDoc->addField(Zend_Search_Lucene_Field::UnIndexed('lastIndexed',time()));
-		$searchDoc->addField(Zend_Search_Lucene_Field::UnIndexed('contentSample',rawurlencode($contentSample)));
-		$searchDoc->addField(Zend_Search_Lucene_Field::UnIndexed('contentHead1',rawurlencode($contentHead1)));
-		$searchDoc->addField(Zend_Search_Lucene_Field::UnIndexed('contentHead2',rawurlencode($contentHead2)));
-		foreach ($this->_fields as $field=>$value) {
-			$searchDoc->addField(Zend_Search_Lucene_Field::Text($field,$value));
+	protected function _indexDocument($doc, $fields) {		
+		$doc->addField(Zend_Search_Lucene_Field::UnIndexed('checkSum',md5($doc->body)));
+		$doc->addField(Zend_Search_Lucene_Field::UnIndexed('lastIndexed',time()));
+		foreach ($fields as $name => $value) {
+			$doc->addField(Zend_Search_Lucene_Field::Text($name, $value));
 		}
-		$this->_index->addDocument($searchDoc);
-		$this->_report('Added '.urldecode($searchDoc->url).' to index');
+		$this->_index->addDocument($doc);
+		Bbx_Log::debug('Added ' . urldecode($doc->url) . ' to index');
 	}
 	
 	protected function _deleteDocument($id) {
-		$this->_report('Deleting document id '.$id);
+		Bbx_Log::debug('Deleting document id '.$id);
 		$this->_index->delete($id);
 	}
 	
-	protected function _getIndex() {
-		$index = SITE_ROOT.'/search/index';
-		try {
-			$this->_index = Zend_Search_Lucene::open($index);
-		}
-		catch (Exception $e) {
-			$this->_index = Zend_Search_Lucene::create($index);
-		}
-	}
-	
-	protected function _checkDocumentExists($docId) {
-		$hits = $this->_index->find('docId:'.$docId);
+	protected function _documentExists($docId) {
+		@$hits = $this->_index->find('docId:' . $docId);
 		foreach ($hits as $hit) {
 			if ($hit->docId == $docId) {
-				$this->_report('Found existing document: '.urldecode($this->_fields['url']));
+				Bbx_Log::debug('Found existing document: ' . urldecode($hit->url));
 				return $hit;
 			}
 		}
@@ -130,15 +116,12 @@ class Bbx_Search {
 		$results = $this->_index->find($this->_query);
 		$preparedResults = array();
 		foreach ($results as $result) {
-			$url = urldecode(reset(explode(urlencode('?'),$result->url)));
-			if (!isset($preparedResults[$url]) && $result->contentSample != '') {
+			$url = urldecode($result->url);
+			if (!array_key_exists($url, $preparedResults)) {
 				$preparedResults[$url] = array(
 					'title'=>$result->title,
-					'lang'=>$result->lang,
+					'contentSample'=>$result->contentSample,
 					'score'=>$result->score,
-					'contentSample'=>rawurldecode($result->contentSample),
-					'contentHead1'=>rawurldecode($result->contentHead1),
-					'contentHead2'=>rawurldecode($result->contentHead2),
 					'docId'=>$result->docId,
 				);
 			}
