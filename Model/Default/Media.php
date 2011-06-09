489789bc43f1acc420558f999624e830c3b5addf
robertdallasgray <robertdallasgray@8c6fc3d7-bd60-4b29-84f5-874647381e5f>
2011-06-09 12:13:47 +0000

    CDN functionality.
    git-svn-id: file://localhost/Users/robertdallasgray/svn/Backbox/trunk@498 8c6fc3d7-bd60-4b29-84f5-874647381e5f

--- Model/Default/Media.php
+++ Model/Default/Media.php
@@ -21,30 +21,55 @@ class Bbx_Model_Default_Media extends Bbx_Model {
 	protected $_mimeType;
 	protected $_mediaPath;
 	protected $_extension;
+	protected $_cdnType;
+	protected $_cdn;
+
+	public function __construct() {
+		parent::__construct();
+		if (isset($this->_cdnType)) {
+			$this->_initCdn();
+		}
+	}
+	
+	protected function _initCdn() {
+		switch($this->_cdnType) {
+			case 's3':
+			$this->_cdn = Bbx_Model_Default_Media_Cdn_S3::init();
+			break;
+		}
+	}
 	
 	public function getMimeType() {
 		return $this->_mimeType;
 	}
 	
-	public function getMediaPath() {
-		return APPLICATION_PATH . '/..' . $this->_mediaPath . $this->id . '.' . $this->_extension;
+	public function getMediaPath($forceLocal = false) {
+		$path = $this->_mediaPath . $this->id . '.' . $this->_extension;
+		return $this->_mediaPath($path, $forceLocal);
 	}
 	
+	protected function _mediaPath($path, $forceLocal = false) {
+		if (!$forceLocal && isset($this->_cdn) && !is_readable(APPLICATION_PATH . '/..' . $path)) {
+			return $this->_cdn->filePath($path);
+		}
+		return APPLICATION_PATH . '/..' . $path;
+	}
+		
 	public function getMediaUrl($absolute = false) {
+		$path = $this->_mediaUrl . $this->id . '.' . $this->_extension;
 		$absolutePath = $absolute ? 'http://'.$_SERVER['SERVER_NAME'] : '';
-		return $absolutePath . $this->_mediaUrl . $this->id . '.' . $this->_extension;
+		return $absolutePath . $$path;
 	}
 	
 	public function getExtension() {
 		return $this->_extension;
 	}
 	
-	
 	public function attachMedia($filePath, $overwrite = true) {
 		Bbx_Log::debug("trying to attach media to " . get_class($this) . ' ' . $this->id);
 		
-		if (!copy($filePath, $this->getMediaPath())) {
-			throw new Bbx_Model_Exception('Unable to save media ' . $filePath . ' to ' . $this->getMediaPath());
+		if (!copy($filePath, $this->getMediaPath(true))) {
+			throw new Bbx_Model_Exception('Unable to save media ' . $filePath . ' to ' . $this->getMediaPath(true));
 		}
 	}
 	
