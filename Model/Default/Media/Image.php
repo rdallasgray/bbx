1a9f228ca4827c03eef36b8a34a9f877163e1cfe
robertdallasgray <robertdallasgray@8c6fc3d7-bd60-4b29-84f5-874647381e5f>
2011-06-09 14:38:46 +0000

    testing s3
    git-svn-id: file://localhost/Users/robertdallasgray/svn/Backbox/trunk@501 8c6fc3d7-bd60-4b29-84f5-874647381e5f

--- Model/Default/Media/Image.php
+++ Model/Default/Media/Image.php
@@ -82,6 +82,9 @@ class Bbx_Model_Default_Media_Image extends Bbx_Model_Default_Media {
 		}
 		$this->setSize($size);
 		$path = str_replace(':size', $size, $this->_mediaUrl) . '/' . $this->id . '.' . $this->_extension;
+		if (isset($this->_cdn) && !is_readable(APPLICATION_PATH . '/..' . $path)) {
+			return $this->_cdn->filePath($path);
+		}
 		$absolutePath = $absolute ? 'http://'.$_SERVER['SERVER_NAME'] : '';
 		return $absolutePath . $path;
 	}
