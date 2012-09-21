665385c367648137ddbe593d23a4113fa49eb27e
Robert Dallas Gray <mail@robertdallasgray.com>
2012-09-21 13:51:31 +0100

    Proceed with cdn sync if sentinel file more than a day old.

--- bin/cdn-sync.php
+++ bin/cdn-sync.php
@@ -7,30 +7,33 @@ Bbx_Log::write('Starting CDN Sync');
 $sentinel_file = APPLICATION_PATH . '/../www/media/.cdn_syncing';
 
 if (file_exists($sentinel_file)) {
-	Bbx_Log::write('CDN is already syncing');
-	exit();
+    if (time() - filemtime($sentinel_file) < 86400) {
+        Bbx_Log::write('CDN is already syncing');
+        exit();
+    }
 }
 
 if (!touch($sentinel_file)) {
-  Bbx_Log::write('Unable to create sentinel file for CDN sync');
+    Bbx_Log::write('Unable to create sentinel file for CDN sync');
 }
+else {
+    set_time_limit(7200);
+    $start = (string) $argv[1];
 
-set_time_limit(7200);
-$start = (string) $argv[1];
+    $cdnType = (string) $argv[2];
 
-$cdnType = (string) $argv[2];
+    Bbx_Log::write('CDN syncing at ' . $start . ', CDN type is ' . $cdnType);
 
-Bbx_Log::write('CDN syncing at ' . $start . ', CDN type is ' . $cdnType);
+    switch($cdnType) {
+    case 's3':
+        $cdn = Bbx_Model_Default_Media_Cdn_S3::init();
+    }
 
-switch($cdnType) {
-	case 's3':
-	$cdn = Bbx_Model_Default_Media_Cdn_S3::init();
-}
+    $cdn->sync($start);
 
-$cdn->sync($start);
-
-if (!unlink($sentinel_file)) {
-  Bbx_Log::write('Unable to delete sentinel file for CDN sync');
+    if (!unlink($sentinel_file)) {
+        Bbx_Log::write('Unable to delete sentinel file for CDN sync');
+    }
 }
 
 ?>
