2634082f45ca3726c60d3261c471b787a5de84d7
robertdallasgray <robertdallasgray@8c6fc3d7-bd60-4b29-84f5-874647381e5f>
2012-10-02 15:59:55 +0000

    Removed check for index or show actions before doRequestMethod; it would prevent e.g. POSTing to rel=images if an imagesAction exists in the controller.
    git-svn-id: file://localhost/Users/robertdallasgray/svn/Backbox/trunk@588 8c6fc3d7-bd60-4b29-84f5-874647381e5f

--- ActionHelper/Media.php
+++ ActionHelper/Media.php
@@ -2,76 +2,76 @@
 
 /*
 
-Copyright (c) 2009 Robert Johnston
+  Copyright (c) 2009 Robert Johnston
 
-This file is part of Backbox.
+  This file is part of Backbox.
 
-Backbox is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software  Foundation, either version 3 of the License, or (at your option) any later version.
+  Backbox is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software  Foundation, either version 3 of the License, or (at your option) any later version.
 
-Backbox is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+  Backbox is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License along with Backbox. If not, see http://www.gnu.org/licenses/.
+  You should have received a copy of the GNU General Public License along with Backbox. If not, see http://www.gnu.org/licenses/.
 
 */
 
 class Bbx_ActionHelper_Media extends Zend_Controller_Action_Helper_Abstract {
 
 
-	public function handleUpload($collection) {
-		$request = $this->getRequest();
-		$response = $this->getResponse();
-		if ($request->getHeader("Content-Length") == 0) {
-			$response->setHttpResponseCode(204)->sendResponse();
-			exit();
-		}
+    public function handleUpload($collection) {
+        $request = $this->getRequest();
+        $response = $this->getResponse();
+        if ($request->getHeader("Content-Length") == 0) {
+            $response->setHttpResponseCode(204)->sendResponse();
+            exit();
+        }
 
-		$mimeType = Bbx_Model::load($collection->getModelName())->getMimeType();
+        $mimeType = Bbx_Model::load($collection->getModelName())->getMimeType();
 
-		$max_size = ini_get('upload_max_filesize');
-		$post_max = ini_get('post_max_size');
-		if ($post_max < $max_size) {
-			$max_size = $post_max;
-		}
+        $max_size = ini_get('upload_max_filesize');
+        $post_max = ini_get('post_max_size');
+        if ($post_max < $max_size) {
+            $max_size = $post_max;
+        }
 
-		$upload = new Zend_File_Transfer_Adapter_Http();
-		// TODO throws error 'Magicfile can not be set. There is no finfo extension installed' (Zend_Validate_File_MimeType 193)		
-		//		$upload->addValidator('MimeType', false, $mimeType);
-		$upload->getValidator('Zend_Validate_File_Upload')
-			->setMessage('File is too large - max size ' . $max_size, Zend_Validate_File_Upload::INI_SIZE);
+        $upload = new Zend_File_Transfer_Adapter_Http();
+        // TODO throws error 'Magicfile can not be set. There is no finfo extension installed' (Zend_Validate_File_MimeType 193)		
+        //		$upload->addValidator('MimeType', false, $mimeType);
+        $upload->getValidator('Zend_Validate_File_Upload')
+            ->setMessage('File is too large - max size ' . $max_size, Zend_Validate_File_Upload::INI_SIZE);
 
-		if ($upload->receive()) {
+        if ($upload->receive()) {
 
-			Bbx_Log::debug("upload received");
+            Bbx_Log::debug("upload received");
 
-			$files = $upload->getFileInfo();
-			$media = $files['file_data'];
+            $files = $upload->getFileInfo();
+            $media = $files['file_data'];
 
-			Bbx_Log::debug(print_r($media,true));
+            Bbx_Log::debug(print_r($media,true));
 
-			$new_model = $collection->create();
-			try {
-				$new_model->attachMedia($media['tmp_name']);
-				$new_model->save();
-			}
-			catch (Exception $e) {
-				$new_model->delete();
-				throw $e;
-			}
-			return $new_model;
-		}
-		else {
-			$this->_throwUploadException($upload);
-		}
-	}
+            $new_model = $collection->create();
+            try {
+                $new_model->attachMedia($media['tmp_name']);
+                $new_model->save();
+            }
+            catch (Exception $e) {
+                $new_model->delete();
+                throw $e;
+            }
+            return $new_model;
+        }
+        else {
+            $this->_throwUploadException($upload);
+        }
+    }
 
-	protected function _throwUploadException($upload) {
-		$msgs = $upload->getMessages();
-		if (empty($msgs)) {
-			$msgs = array('Unknown failure - please contact support.');
-		}
-		Bbx_Log::debug('Upload failed: '.implode($msgs));
-		throw new Bbx_Controller_Rest_Exception('Upload failed: '.implode($msgs),500);
-	}
+    protected function _throwUploadException($upload) {
+        $msgs = $upload->getMessages();
+        if (empty($msgs)) {
+            $msgs = array('Unknown failure - please contact support.');
+        }
+        Bbx_Log::debug('Upload failed: '.implode($msgs));
+        throw new Bbx_Controller_Rest_Exception('Upload failed: '.implode($msgs),500);
+    }
 
 }
 
--- Controller/Rest/Crud.php
+++ Controller/Rest/Crud.php
@@ -2,15 +2,15 @@
 
 /*
 
-Copyright (c) 2009 Robert Johnston
+  Copyright (c) 2009 Robert Johnston
 
-This file is part of Backbox.
+  This file is part of Backbox.
 
-Backbox is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software  Foundation, either version 3 of the License, or (at your option) any later version.
+  Backbox is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software  Foundation, either version 3 of the License, or (at your option) any later version.
  
-Backbox is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+  Backbox is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License along with Backbox. If not, see http://www.gnu.org/licenses/.
+  You should have received a copy of the GNU General Public License along with Backbox. If not, see http://www.gnu.org/licenses/.
 
 */
 
@@ -18,138 +18,136 @@ You should have received a copy of the GNU General Public License along with Bac
 
 class Bbx_Controller_Rest_Crud extends Bbx_Controller_Rest {
 
-	public function preDispatch() {
-		parent::preDispatch();
-		$request = $this->getRequest();
-		if (($rel = $request->getParam('rel'))) {
-			if (method_exists($this, Inflector::camelize($rel) . 'Action') && !$request->getParam('final'))  {
-				$request->setActionName($rel)->setParam('final', true)->setDispatched(false);
-				return;
-			}
-			Zend_Controller_Action_HelperBroker::getExistingHelper('viewRenderer')->setScriptAction($rel);
-		}
-		if(in_array($request->getActionName(), array('index', 'show'))) {
-		  $this->_doRequestMethod();
-		}
-		Zend_Controller_Front::getInstance()->getResponse()->setHeader('Cache-control', 'must-revalidate', true);
-	}
+    public function preDispatch() {
+        parent::preDispatch();
+        $request = $this->getRequest();
+        if (($rel = $request->getParam('rel'))) {
+            if (method_exists($this, Inflector::camelize($rel) . 'Action') && !$request->getParam('final'))  {
+                $request->setActionName($rel)->setParam('final', true)->setDispatched(false);
+                return;
+            }
+            Zend_Controller_Action_HelperBroker::getExistingHelper('viewRenderer')->setScriptAction($rel);
+        }
+        $this->_doRequestMethod();
+        Zend_Controller_Front::getInstance()->getResponse()->setHeader('Cache-control', 'must-revalidate', true);
+    }
 	
-	public function indexAction() {
-		$collection = $this->_getIndexData();
-		$this->_assign($collection);
-	}
+    public function indexAction() {
+        $collection = $this->_getIndexData();
+        $this->_assign($collection);
+    }
 
-	public function showAction() {
-		$model = $this->_getShowData();
-		$this->_assign($model);
-	}
+    public function showAction() {
+        $model = $this->_getShowData();
+        $this->_assign($model);
+    }
 	
-	public function newAction() {
-		$this->_helper->authenticate();
-		$model = $this->_helper->Model->getModel();
-		$this->view->assign($model->newModel());
-	}
+    public function newAction() {
+        $this->_helper->authenticate();
+        $model = $this->_helper->Model->getModel();
+        $this->view->assign($model->newModel());
+    }
 	
-	protected function _getIndexData() {
-		return $this->_helper->Model->getCollection();
-	}
+    protected function _getIndexData() {
+        return $this->_helper->Model->getCollection();
+    }
 	
-	protected function _getShowData() {
-		return $this->_helper->Model->getModel();
-	}
+    protected function _getShowData() {
+        return $this->_helper->Model->getModel();
+    }
 	
-	protected function _doRequestMethod() {
-		$method = '_' . strtolower($this->getRequest()->getMethod());
-		if (method_exists($this, $method)) {
-			$this->$method();
-		}
-	}
+    protected function _doRequestMethod() {
+        $method = '_' . strtolower($this->getRequest()->getMethod());
+        if (method_exists($this, $method)) {
+            $this->$method();
+        }
+    }
 
-	protected function _assign($model) {
-		if ($this->getRequest()->isHead()) {
-			Zend_Controller_Action_HelperBroker::getExistingHelper('viewRenderer')->setNoRender(true);
-			return;
-		}
-		$request = $this->getRequest();
-		$this->_setEtag($model->etag($this->_context));
+    protected function _assign($model) {
+        if ($this->getRequest()->isHead()) {
+            Zend_Controller_Action_HelperBroker::getExistingHelper('viewRenderer')->setNoRender(true);
+            return;
+        }
+        $request = $this->getRequest();
+        $this->_setEtag($model->etag($this->_context));
 
-		$modelName = $model instanceof Bbx_Model ? 
-				Inflector::underscore(get_class($model)) : Inflector::tableize($model->getModelName());
+        $modelName = $model instanceof Bbx_Model ? 
+            Inflector::underscore(get_class($model)) : Inflector::tableize($model->getModelName());
 	
-		if ($request->getParam('list') === 'true') {
-			$model->renderAsList();
-		}		
-		if ($this->_context === 'csv') {
-			$this->_helper->authenticate();
-		}
-		if ($request->getParam('download') == 'true') {
-			$this->_helper->authenticate();
-			$this->_helper->download($model);
-		}
+        if ($request->getParam('list') === 'true') {
+            $model->renderAsList();
+        }		
+        if ($this->_context === 'csv') {
+            $this->_helper->authenticate();
+        }
+        if ($request->getParam('download') == 'true') {
+            $this->_helper->authenticate();
+            $this->_helper->download($model);
+        }
 
-		if ($this->_context === 'json') {
-			$options = ($this->_context === 'json') ? array('deep' => true) : null;
-			$this->view->assign($model->toArray($options));
-		}
-		else {
-			$this->view->$modelName = $model;
-		}
-	}
+        if ($this->_context === 'json') {
+            $options = ($this->_context === 'json') ? array('deep' => true) : null;
+            $this->view->assign($model->toArray($options));
+        }
+        else {
+            $this->view->$modelName = $model;
+        }
+    }
 
-	protected function _post() {
-		$this->_helper->authenticate();
-		$collection = $this->_helper->Model->getCollection();
-		if ($collection instanceof Bbx_Model) {
-			// It's a hasOne-type model
-			throw new Bbx_Controller_Rest_Exception(null, 405, array('allowed_methods' => 'GET,PUT'));
-		}
-		$model = Bbx_Model::load($collection->getModelName());
-		if ($model instanceof Bbx_Model_Default_Media) {
-			$new_model = $this->_helper->Media->handleUpload($collection);
-		}
-		else {
-			$new_model = $collection->create($this->_getBodyData());
-		}
+    protected function _post() {
+        $this->_helper->authenticate();
+        $collection = $this->_helper->Model->getCollection();
+        if ($collection instanceof Bbx_Model) {
+            // It's a hasOne-type model
+            throw new Bbx_Controller_Rest_Exception(null, 405, array('allowed_methods' => 'GET,PUT'));
+        }
+        $model = Bbx_Model::load($collection->getModelName());
+        if ($model instanceof Bbx_Model_Default_Media) {
+            $new_model = $this->_helper->Media->handleUpload($collection);
+        }
+        else {
+            $new_model = $collection->create($this->_getBodyData());
+        }
 		
-		Bbx_CacheManager::clean($this->getRequest(), 'post');
+        Bbx_CacheManager::clean($this->getRequest(), 'post');
 		
-		$this->getResponse()->setHttpResponseCode(201)
-			->setHeader('Location',$new_model->url(true))
-			->setBody(Zend_Json::encode($new_model->toArray()))
-			->sendResponse();
-		//TODO if we exit, we can't use postDispatch hooks ...	also, this presumes we always want to respond with JSON
-		exit();
-	}
+        $this->getResponse()->setHttpResponseCode(201)
+            ->setHeader('Location',$new_model->url(true))
+            ->setBody(Zend_Json::encode($new_model->toArray()))
+            ->sendResponse();
+        //TODO if we exit, we can't use postDispatch hooks ...	also, this presumes we always want to respond with JSON
+        exit();
+    }
 
-	protected function _put($data = null) {
-		$this->_helper->authenticate();
-		try {
-			$model = $this->_helper->Model->getModel();
-			$method = ($model->isEmpty()) ? 'create' : 'update';
-			if ($data === null) {
-				$data = $this->_getBodyData();
-			}
-			$model->$method($data);
-			Bbx_CacheManager::clean($this->getRequest(), 'put');
-		}
-		catch (Exception $e) {
-			Bbx_Log::write('Unable to _put to model: ' . $e->getMessage());
-			throw new Bbx_Controller_Rest_Exception(null, 405, array('allowed_methods' => 'GET,POST'));
-		}
-	}
+    protected function _put($data = null) {
+        $this->_helper->authenticate();
+        try {
+            $model = $this->_helper->Model->getModel();
+            $method = ($model->isEmpty()) ? 'create' : 'update';
+            if ($data === null) {
+                $data = $this->_getBodyData();
+            }
+            $model->$method($data);
+            Bbx_CacheManager::clean($this->getRequest(), 'put');
+        }
+        catch (Exception $e) {
+            Bbx_Log::write('Unable to _put to model: ' . $e->getMessage());
+            throw new Bbx_Controller_Rest_Exception(null, 405, array('allowed_methods' => 'GET,POST'));
+        }
+    }
 
-	protected function _delete() {
-		$this->_helper->authenticate();
-		if (!$this->_hasParam('id')) {
-			$e = new Bbx_Controller_Rest_Exception(null,405,array('allowed_methods'=>'GET,POST'));
-			throw $e;
-		}
-		$model = $this->_helper->Model->getModel();
-		$model->delete($this->_getParam('id'));
-		Bbx_CacheManager::clean($this->getRequest(), 'delete');
-		$this->getResponse()->setHttpResponseCode(204)->sendResponse();
-		exit();
-	}
+    protected function _delete() {
+        $this->_helper->authenticate();
+        if (!$this->_hasParam('id')) {
+            $e = new Bbx_Controller_Rest_Exception(null,405,array('allowed_methods'=>'GET,POST'));
+            throw $e;
+        }
+        $model = $this->_helper->Model->getModel();
+        $model->delete($this->_getParam('id'));
+        Bbx_CacheManager::clean($this->getRequest(), 'delete');
+        $this->getResponse()->setHttpResponseCode(204)->sendResponse();
+        exit();
+    }
 
 }
 
